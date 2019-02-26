4e2e37e22a80f6416771884cc9d78095a000e2e2
Owner <Owner@Sami-DESKTOP>
2019-02-25 22:48:31 -0700

    Step 2

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,7 +1,7 @@
 package cse360assign2;
 
 public class Calculator {
-private int total;
+	private int total;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
@@ -12,19 +12,23 @@ private int total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
 	}
 	
 	public void multiply (int value) {
-		
+		total *= value;
 	}
 	
 	public void divide (int value) {
+		total /= value;
 		
+		if(value == 0) {
+			total = 0;
+		}
 	}
 	
 	public String getHistory () {
